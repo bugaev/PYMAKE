1eeccc9ddfd26588e132b8969e46fed3e4c84b7a
Viatcheslav Bugaev <viatcheslav.bugayov@gmail.com>
2012-04-28 10:03:14 -0500

    A problem with a longer tree solved successfully.
    The tree shown in the ascii drawing in test_make.py was used to test
    ability of the make utility to rebuild missing nodes when they are
    needed by nodes in upper layers of the tree.

--- test_make.py
+++ test_make.py
@@ -27,17 +27,22 @@ def rule_touch(target, prereq):
 PyMake.queues[rule_touch] = "express"
 
 
-PyMake.rules['T'] = rule_touch
-PyMake.rules['t9'] = rule_touch
-PyMake.rules['t8'] = rule_touch
-PyMake.rules['t7'] = rule_touch
-PyMake.rules['t6'] = rule_touch
-PyMake.rules['t5'] = rule_touch
-#PyMake.rules['t4'] = rule_touch
-#PyMake.rules['t3'] = rule_touch
-#PyMake.rules['t2'] = rule_touch
-#PyMake.rules['t1'] = rule_touch
 PyMake.rules['t0'] = rule_touch
+PyMake.rules['t1'] = rule_touch
+PyMake.rules['t2'] = rule_touch
+PyMake.rules['t3'] = rule_touch
+PyMake.rules['t4'] = rule_touch
+PyMake.rules['t5'] = rule_touch
+PyMake.rules['t6'] = rule_touch
+PyMake.rules['t7'] = rule_touch
+PyMake.rules['t8'] = rule_touch
+PyMake.rules['t9'] = rule_touch
+PyMake.rules['t10'] = rule_touch # Bottom node.
+#PyMake.rules['t11'] = rule_touch # Bottom node.   
+#PyMake.rules['t12'] = rule_touch # Bottom node.
+#PyMake.rules['t13'] = rule_touch # Bottom node.
+#PyMake.rules['t15'] = rule_touch # Bottom node.
+PyMake.rules['t14'] = rule_touch
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  END OF RULES  ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 PyMake.dorule(dir, globals())
@@ -47,17 +52,21 @@ PyMake.dorule(dir, globals())
 ##################           BEGIN DEPENDENCIES         ################
 ########################################################################
 ########################################################################
-PyMake.depend['T'] = ['t9']
-PyMake.depend['t9'] = ['t6', 't8', 't0']
-PyMake.depend['t8'] = ['t5']
-PyMake.depend['t7'] = ['t3', 't4']
-PyMake.depend['t5'] = ['t1', 't2']
-PyMake.depend['t6'] = ['t5']
-PyMake.depend['t0'] = ['t7']
+PyMake.depend['t0'] = ['t1', 't2', 't3']
+PyMake.depend['t1'] = ['t8', 't4']
+PyMake.depend['t2'] = ['t9', 't5']
+PyMake.depend['t3'] = ['t6', 't7']
+PyMake.depend['t4'] = ['t9']
+PyMake.depend['t5'] = ['t14']
+PyMake.depend['t6'] = ['t14']
+PyMake.depend['t7'] = ['t10']
+PyMake.depend['t8'] = ['t11']
+PyMake.depend['t9'] = ['t12']
+PyMake.depend['t14'] = ['t13', 't15']
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^ END OF DEPENDENCIES  ^^^^^^^^^^^^^^^^^^^^^^
 
 
-PyMake.make('T', MAX_PARALLEL_JOBS, dir)
+PyMake.make('t0', MAX_PARALLEL_JOBS, dir)
 
 sys.exit()
 
